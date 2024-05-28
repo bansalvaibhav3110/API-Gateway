43c6c6035940114f8481c881cd70d354a160a835
Vaibhav Bansal <vaibhav.bansal@habilelabs.io>
2024-05-28 10:08:17 +0530

    basic setup of api gateway done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const {serverConfig,logger} = require('./config');
+const { serverConfig } = require('./config');
 const apiRoutes = require('./routes')
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use('/api',apiRoutes)
 
 app.listen(serverConfig.PORT,()=>{
     console.log(`successfully started the server  on port: ${serverConfig.PORT} `);
-    logger.info('Succsfully started the server')
+   
 }) 
-console.log("clkdj")
