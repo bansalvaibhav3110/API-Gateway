64a8f89049bce77f8998f9dadf5cf61ace785c91
Vaibhav Bansal <vaibhav.bansal@habilelabs.io>
2024-05-28 20:44:32 +0530

    added middleware for auth

--- src/routes/v1/user-routes.js
+++ src/routes/v1/user-routes.js
@@ -1,15 +1,18 @@
 const express = require('express');
 
 const router = express.Router();
+const { AuthRequestMiddleware } = require('../../middlewares')
 
 const { UserController } = require('../../controllers')
 
 router.post(
     '/signup',
+    AuthRequestMiddleware.validateAuthRequest,
     UserController.signup
 )
 router.post(
     '/signin',
+    AuthRequestMiddleware.validateAuthRequest,
     UserController.signin
 )
 
